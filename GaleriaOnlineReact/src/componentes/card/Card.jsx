c9de07aa45c38235c381c089ac1bd5c6c4791b06
VINÍCIUS BERNARDO POLIMENO <47766024829@info.local>
2025-08-26 14:51:24 -0300

    feat: Ajustando funcionabilidade do crud funcionando testado

--- GaleriaOnlineReact/src/componentes/card/Card.jsx
+++ GaleriaOnlineReact/src/componentes/card/Card.jsx
@@ -1,19 +1,18 @@
 import "./Card.css"
-
-import imgCard from "../../assets/img/panteracorderosa.jpg"
+// import imgCard from "../../assets/img/panteracorderosa.jpg"
 
 import imgPen from "../../assets/img/pen.svg"
 import imgTrash from "../../assets/img/trash.svg"
 
-export const Card = ({tituloCard}) => {
+export const Card = ({tituloCard, imgCard, funcaoEditar, funcaoExcluir}) => {
     return(
         <>
             <div className="cardDaImagem">
                 <p>{tituloCard}</p>
                 <img className="imgDoCard" src={imgCard} alt="imagem relacionada ao card." />
                 <div className="icons">
-                    <img src={imgPen} alt="Icone de caneta para realizar uma alteração." />
-                    <img src={imgTrash} alt="ícone de uma lixeira para realizar a exclusão." />
+                    <img onClick={funcaoEditar} src={imgPen} alt="Icone de caneta para realizar uma alteração." />
+                    <img onClick={funcaoExcluir} src={imgTrash} alt="ícone de uma lixeira para realizar a exclusão." />
                 </div>
             </div>
         </>
---
+++ GaleriaOnlineReact/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import "Home.css"
+import { Botao } from "../../componentes/botao/Botao"
+export const Home = () => {
+  return (
+    <>
+        <h3>Bem-vindo a</h3>
+        <h1>Galeria Online</h1>
+
+        <Botao nomeBotao="Entrar"/>
+    </>
+  )
+}
+
